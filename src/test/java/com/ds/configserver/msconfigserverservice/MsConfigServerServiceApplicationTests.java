429668353cfb6c555e07a3e83810ac1b70af69dd
Divanio Silva <dcsilva@mail.ru>
2022-07-20 20:45:36 +0200

    First commit

---
+++ src/test/java/com/ds/configserver/msconfigserverservice/MsConfigServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.configserver.msconfigserverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigServerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
